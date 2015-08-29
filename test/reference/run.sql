af0ea920faa507c25ae04ee46714074c86127ef4
Stefan Oravec <stefan.oravec@me.com>
2015-08-29 21:23:53 +0200

    reference tests - type

--- test/reference/run.sql
+++ test/reference/run.sql
@@ -3,4 +3,10 @@
 prompt Reference TAPI package - insert method tests
 @@ut_px_tapir_ref_ins.pkg
 
+prompt Reference TAPI package - convert method tests
+@@ut_px_tapir_ref_convert.pkg
+
+prompt Reference TAPI type
+@@ut_tx_tapir_ref.pkg
+
 @&&run_dir_end
